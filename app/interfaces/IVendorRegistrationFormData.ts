961264cb589513eff0f1e8b4b4e47d06fad34d3b
David Frederick <dfrederick79@gmail.com>
2024-11-19 19:52:34 -0600

    IVendorRegistrationFormData

--- app/interfaces/IVendorRegistrationFormData.ts
+++ app/interfaces/IVendorRegistrationFormData.ts
@@ -1,3 +1,18 @@
+import { IProduct } from "./IProduct";
+
 export interface IVendorRegistrationFormData {
     FirstName: string;
+    LastName: string;
+    Email: string;
+    Phone: string;
+    Street1: string;
+    Street2: string;
+    City: string;
+    State: string;
+    ZipCode: string;
+    BusinessName: string;
+    ProductsYouSell: IProduct[];
+    PreferredSpotNumber: string;
+    SpotSizeOrLocation: string;
+    AgreeToPayUponArrival: boolean;
 }
