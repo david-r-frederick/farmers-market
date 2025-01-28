d4a7a12434ded166f1ec16752ef1ad3bcdd32197
David Frederick <david.frederick@claritymis.com>
2025-01-27 21:20:50 -0600

    GeographyService initial commit

---
+++ app/services/GeographyService.ts
@@ -0,0 +1,29 @@
+import { IAddress } from "~/interfaces/IAddress";
+import { IGeographyService } from "./_interfaces/IGeographyService";
+
+class GeographyService implements IGeographyService {
+  getBlankAddress = (): IAddress => {
+    return {
+      ID: -1,
+      CreatedDate: new Date().toISOString(),
+      IsActive: true,
+      IsDeleted: false,
+      Key: "",
+      City: "",
+      State: "",
+      Street1: "",
+      Street2: "",
+      ZipCode: "",
+    };
+  };
+
+  getCountyFromZipCode = async (zipCode: string): Promise<string> => {
+    // TODO use third-party service to convert zipCode to county name
+    await Promise.resolve(zipCode);
+    return "";
+  };
+}
+
+const geographyService = new GeographyService();
+
+export default geographyService;
