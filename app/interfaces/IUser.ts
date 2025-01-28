10e488bf6a8dc7d1f76c5eaa674f20ef144a139d
David Frederick <david.frederick@claritymis.com>
2025-01-27 21:32:28 -0600

    IUser properties are in alphabetical order

--- app/interfaces/IUser.ts
+++ app/interfaces/IUser.ts
@@ -1,13 +1,13 @@
 import { IBaseModel } from "./IBaseModel";
 
 export interface IUser extends IBaseModel {
+  City: string;
+  County: string;
   FirstName: string;
-  MiddleInitial: string;
   LastName: string;
+  MiddleInitial: string;
+  PostalCode: string;
+  State: string;
   Street1: string;
   Street2: string;
-  City: string;
-  State: string;
-  County: string;
-  PostalCode: string;
 }
