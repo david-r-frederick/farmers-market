5f2ec69d32a2f441346be357fd024a690b950732
David Frederick <david.frederick@claritymis.com>
2025-03-10 06:33:59 -0500

    Initial build out of event registration index.tsx

---
+++ app/routes/event-registration/index.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { EventRegistrationForm } from "~/components/forms/EventRegistrationForm";
+import { useUIStatus } from "~/hooks/useUIStatus";
+import { IEventRegistrationFormData } from "~/interfaces/IEventRegistrationFormData";
+import eventsService from "~/services/EventsService";
+
+const EventRegistrationPage = (): JSX.Element => {
+  const [ didSave, setDidSave ] = useState<boolean>(false);
+
+  const {
+    UIStatus,
+    beginProcessing,
+    endProcessing
+  } = useUIStatus();
+
+  const handleEventRegistrationFormSubmit = (formData: IEventRegistrationFormData): void => {
+    // a separate function so I can use async/await
+    const doSubmit = async () => {
+      beginProcessing();
+      try {
+        const eventRegistrationDto = eventsService.getEventRegistrationDtoFromFormData(formData);
+        await eventsService.submitEventRegistration(eventRegistrationDto);
+        setDidSave(true);
+        endProcessing();
+      } catch (err) {
+        endProcessing(err as Error);
+      }
+    };
+    doSubmit();
+  };
+
+  return (
+    <div className="bg-secondary py-8">
+      <div className="max-w-lg mx-auto p-6">
+        <EventRegistrationForm onSubmit={handleEventRegistrationFormSubmit} />
+        {UIStatus.processing && <span>Loading...</span>}
+        {didSave && <span>Saved!</span>}
+      </div>
+    </div>
+  );
+};
+
+export default EventRegistrationPage;
