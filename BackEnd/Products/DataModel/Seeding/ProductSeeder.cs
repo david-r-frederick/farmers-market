e35f5ffd57fce5e12725ecde60b2eadfae4ac302
David Frederick <dfrederick79@gmail.com>
2025-05-06 09:33:05 -0500

    ProductSeeder seeding can be disabled with app setting

--- BackEnd/Products/DataModel/Seeding/ProductSeeder.cs
+++ BackEnd/Products/DataModel/Seeding/ProductSeeder.cs
@@ -1,9 +1,28 @@
 ﻿namespace Products.Seeders;
 
+using Core;
+using Core.Repository;
 using Products.DataModel.Entities;
 
 public class ProductSeeder : Seeder<Product>
 {
+    private ISettingsRepository _settingsRepository;
+
+    public ProductSeeder(ISettingsRepository settingsRepository)
+    {
+        _settingsRepository = settingsRepository;
+    }
+
+    public override async Task SeedAsync(IDatabaseContext context)
+    {
+        var seedSampleProductsSetting = await _settingsRepository.GetByKeyAsync("SeedSampleProducts");
+        if (seedSampleProductsSetting is null || seedSampleProductsSetting.Value != "true")
+        {
+            return;
+        }
+        await base.SeedAsync(context);
+    }
+
     public override List<string> GetSeedData() => ["General"];
 
     public override Product MapCustomFieldsToSeedData(string seedKey, Product product)
